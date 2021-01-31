4fda4d302b19d0cbd28400f3157b2ad29f206c02
Ross Rosen <rrosen326@gmail.com>
2021-01-31 06:03:18 -0800

    (split)tweaks

--- adplus/utils.py
+++ adplus/utils.py
@@ -19,16 +19,16 @@ class UpdateStateMixin(_Loggable):
 
         update_state(self, entity, state="value", attributes={})
         """
-        current = await self.get_state(entity, attribute="all") # type: ignore
+        current = await self.get_state(entity, attribute="all")  # type: ignore
         if current == None:
-            current = await self.set_state(entity, state="off") # type: ignore
+            current = await self.set_state(entity, state="off")  # type: ignore
             if current == None:
                 self.log(f"Unable to create state for {entity}")
                 return current
         merged_state = state if state else current["state"]
         merged_attributes = current["attributes"].copy()
         merged_attributes.update(attributes)
-        return await self.set_state( # type: ignore
+        return await self.set_state(  # type: ignore
             entity, state=merged_state, attributes=merged_attributes
         )
 
