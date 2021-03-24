022d0d2eb642fa5ee71ac4f2e12aecf28ba26672
Ross Rosen <rrosen326@gmail.com>
2021-03-24 09:04:03 -0700

    (split)Cleaned up logging

--- adplus/state.py
+++ adplus/state.py
@@ -32,3 +32,23 @@ class UpdateStateMixin(_Loggable):
             entity, state=merged_state, attributes=merged_attributes
         )
 
+class EnsureStateMixin(_Loggable):
+    @sync_wrapper
+    async def ensure_state(self, entity, state=None, attributes={}, success_cb=None, error_cb=None, already_set_cb=None, wait=0.5, retries=2):
+        """
+        Similar to set_state(), except:
+        * Callbacks (success, error, already_set) are called after. Very handy for logging or notifying of success / error
+        * wait - Seconds to wait before testing for success
+        * retries - Number of retries to test for success before declaring error
+
+        Example:
+        def success_cb(self, entity, state, attribute, old, new, kwargs):
+            self.log(f'Successfully set {entity} to state: {state} and attributes: {attributes}.')
+        def error_cb(self, entity, state, attribute, old, new, kwargs, errobj):
+            self.error(f'Failed to set {entity} to state: {state} and attributes: {attributes}. errobj: {errobj}')
+        def already_set_cb(self, entity, state, attribute, old, new, kwargs):
+            pass # State was already at desired state. Don't bother logging.
+
+        new_state = self.set_state('climate.my_ecobee', state="perm_hold", attributes={"temperature":41}, success_cb=success_cb, error_cb=error_cb, wait=3, retries=2)       
+        """
+        raise RuntimeError('Not implemented yet. WIP.')
